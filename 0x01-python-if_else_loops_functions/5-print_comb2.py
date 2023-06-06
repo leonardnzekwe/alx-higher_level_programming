0fc185c8a229e60c783fb8a08f17686b32118c04
Leonard Nzekwe <nzekweleo@gmail.com>
2023-06-06 16:30:58 -0700

    Add Files, task-5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i <= 9:
-        print("0{}, ".format(i), end="")
-    if i > 9 and i <= 98:
-        print("{}, ".format(i), end="")
+    if i <= 98:
+        print("{:02d}, ".format(i), end="")
     if i  == 99:
         print("{}".format(i))
