fb5c9369329a036080b32efaf89535c0781755c5
Leonard Nzekwe <nzekweleo@gmail.com>
2023-06-07 15:21:02 -0700

    Add Files, task-14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('`'), -1):
     if i % 2 == 0:
-        print(chr(i).lower(), end='')
+        print("{}".format(chr(i).lower()), end='')
     else:
-        print(chr(i).upper(), end='')
+        print("{}".format(chr(i).upper()), end='')
 
