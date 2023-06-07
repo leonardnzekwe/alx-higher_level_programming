a9478a24545746f207c404ff56b2be897025a9c3
Leonard Nzekwe <nzekweleo@gmail.com>
2023-06-06 18:34:46 -0700

    Add Files, task-11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    result = 1
-    if b >= 0:
-        for i in range(b):
-            result = result * a
-    else:
-        for i in range(-b):
-            result = result / a
-    return round(result, 20)
+    return a ** b
