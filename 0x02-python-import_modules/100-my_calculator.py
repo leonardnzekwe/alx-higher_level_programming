456e4419b657989f151988e1e6e4720062c2cd69
Leonard Nzekwe <nzekweleo@gmail.com>
2023-06-08 14:19:48 -0700

    Add Files, task-6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv, exit
+    argc = len(argv) - 1
+    if argc != 3:
+        print(f"Usage: {argv[0]} <a> <operator> <b>")
+        exit(1)
+    elif argc == 3:
+        a = int(argv[1])
+        b = int(argv[3])
+        operator = argv[2]
+        if operator != ('+' or '-' or '*' or '/'):
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+        elif operator == '+':
+            print("{} + {} = {}".format(a, b, add(a, b)))
+        elif operator == '-':
+            print("{} - {} = {}".format(a, b, sub(a, b)))
+        elif operator == '*':
+            print("{} * {} = {}".format(a, b, mul(a, b)))
+        elif operator == '/':
+            print("{} / {} = {}".format(a, b, div(a, b)))
