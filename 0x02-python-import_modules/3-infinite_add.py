b444436519968d6a12f3a506e1db22e135cbe7cd
Leonard Nzekwe <nzekweleo@gmail.com>
2023-06-08 10:09:37 -0700

    Add Files, task-3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    argc = len(argv) - 1
+    result = 0
+    if argc >= 2:
+        i = 1
+        result = 0
+        while i - 1 < argc:
+            result = result + int(argv[i])
+            i = i + 1
+    print(result)
