2f81e276db0e04d53b225d7c16c8cef7037cf2ab
Leonard Nzekwe <nzekweleo@gmail.com>
2023-06-12 05:20:03 -0700

    Add Files, task-1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        return None
+        return
     if idx > len(my_list):
-        return None
+        return
     return my_list[idx]
