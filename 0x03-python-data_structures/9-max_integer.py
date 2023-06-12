652db7af43c0dd15fd22a01f9eed69d3943631a3
Leonard Nzekwe <nzekweleo@gmail.com>
2023-06-12 13:35:13 -0700

    Add Files, task-9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    list_len = len(my_list) - 1
+    list_len = len(my_list)
     if list_len == 0:
         return None
     my_list.sort()
-    return my_list[list_len]
+    return my_list[list_len - 1]
