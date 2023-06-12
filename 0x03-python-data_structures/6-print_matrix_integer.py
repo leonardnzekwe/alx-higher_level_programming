85c3274d018e54e90a6d3c9ca5bbeca33ef2c74d
Leonard Nzekwe <nzekweleo@gmail.com>
2023-06-12 12:13:43 -0700

    Add Files, task-6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for value in row:
+            print("{:d}".format(value), end="")
+            if row.index(value) < len(row) - 1:
+                print(" ", end="")
+        print(end="\n")
