6d075e8a997f85609d6f254f260fd545e425fc55
Leonard Nzekwe <nzekweleo@gmail.com>
2023-06-12 11:15:23 -0700

    Add Files, task-4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     cpy_list = my_list.copy()
     if idx < 0:
         return cpy_list
-    if idx > (len(my_list) - 1):
+    if idx > (len(cpy_list) - 1):
         return cpy_list
     cpy_list[idx] = element
     return cpy_list
