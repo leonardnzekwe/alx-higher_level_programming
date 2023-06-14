738636c619cdbba34d0d458c1f7f45188cc5b1e4
Leonard Nzekwe <nzekweleo@gmail.com>
2023-06-14 08:02:17 -0700

    Update Files, task-0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i in my_list:
-        print("{:d}".format(my_list[i - 1]))
+    for item in my_list:
+        print("{:d}".format(item))
