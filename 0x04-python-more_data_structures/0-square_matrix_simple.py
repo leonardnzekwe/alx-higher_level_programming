d84bf961848ac84697197df5272dd8b57498c8f3
Leonard Nzekwe <nzekweleo@gmail.com>
2023-06-14 09:05:12 -0700

    Add Files, task-0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     pow_matrix = []
     for row in matrix:
-        pow_matrix.append(list(map(lambda x : x**2, row)))
+        pow_matrix.append(list(map(lambda x: x**2, row)))
     return pow_matrix
