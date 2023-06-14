ea88a66bde3fd22086909c698d6169926470138b
Leonard Nzekwe <nzekweleo@gmail.com>
2023-06-14 08:05:45 -0700

    Update Files, task-3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if my_list:
-        for i in reversed(my_list):
-            print("{:d}".format(my_list[i - 1]))
+        for rev_item in reversed(my_list):
+            print("{:d}".format(rev_item))
