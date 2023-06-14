f9a51a378fa38cb269cd870ae723cec7ea99356b
Leonard Nzekwe <nzekweleo@gmail.com>
2023-06-14 15:47:06 -0700

    Add Files, task-10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    max_value = 0
+    max_value_key = None
+    for key, value in a_dictionary.items():
+        if value > max_value:
+            max_value = value
+            max_value_key = key
+    return max_value_key
