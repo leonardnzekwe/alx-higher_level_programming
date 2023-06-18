84770860a9192cc7f9257473fc8a0b0c36200a93
Leonard Nzekwe <nzekweleo@gmail.com>
2023-06-18 05:13:49 -0700

    Update Files, task-13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,12 +11,10 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *ptr, *ptr2;
-	int i, j, num_nodes;
-	int *nodes_data;
+	int i, j, num_nodes, *nodes_data;
 
 	if (*head == NULL) /* empty list case section */
 		return (1);
-
 	ptr2 = *head; /* section for dynamic memory alloc for an array of integers */
 	num_nodes = 0;
 	while (ptr2 != NULL)
@@ -27,7 +25,6 @@ int is_palindrome(listint_t **head)
 	nodes_data = malloc(sizeof(int) * num_nodes);
 	if (nodes_data == NULL)
 		return (1);
-
 	ptr = *head; /* section that stores the data of each node in an array */
 	i = 0;
 	while (ptr != NULL)
@@ -36,7 +33,6 @@ int is_palindrome(listint_t **head)
 		i = i + 1;
 		ptr = ptr->next;
 	}
-
 	j = 0; /* section that checks if array values reads same to & fro */
 	i = i - 1;
 	while (i >= 0)
@@ -47,7 +43,11 @@ int is_palindrome(listint_t **head)
 			j = j + 1;
 		}
 		else
+		{
+			free(nodes_data);
 			return (0);
+		}
 	}
+	free(nodes_data);
 	return (1);
 }
