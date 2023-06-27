5e38dd4ae89f477e1e51b79404ff43944504497a
Leonard Nzekwe <nzekweleo@gmail.com>
2023-06-27 14:56:02 -0700

    Add Files, task-6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -59,13 +59,16 @@ class Square:
         if self.size == 0:
             print()
         else:
+            m = 0
+            while m < self.position[1]:
+                print()
+                m += 1
             i = 0
             while i < self.size:
-                if self.position[1] >= 0:
-                    k = 0
-                    while k < self.position[0]:
-                        print(" ", end="")
-                        k += 1
+                k = 0
+                while k < self.position[0]:
+                    print(" ", end="")
+                    k += 1
                 j = 0
                 while j < self.size:
                     print("#", end="")
