c409313f6bb33c31f5a0b527bc6887e0624b1483
Leonard Nzekwe <nzekweleo@gmail.com>
2023-06-27 10:47:35 -0700

    Add Files, task-1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This is 1-square Module"""
+
+
+class Square:
+    """This is Square class"""
+
+    def __init__(self, size):
+        """This is the Initialization method"""
+        self.__size = size
