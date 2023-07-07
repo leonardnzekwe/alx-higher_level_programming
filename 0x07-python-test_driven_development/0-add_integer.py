076ba40600137f90494562fa9f5f2265efba7300
Leonard Nzekwe <nzekweleo@gmail.com>
2023-07-07 15:36:30 -0700

    Add Files, task-0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,12 +10,22 @@ def add_integer(a, b=98):
     """
     A function that adds 2 integers
     """
-    if type(a) not in (int, float):
-        raise TypeError("a must be an integer or float")
-    if type(b) not in (int, float):
-        raise TypeError("b must be an integer or float")
+    if (
+        a is None or
+        not isinstance(a, int) and
+        not isinstance(a, float)
+    ):
+        raise TypeError("a must be an integer")
+    if (
+        b is None or
+        not isinstance(b, int) and
+        not isinstance(b, float)
+    ):
+        raise TypeError("b must be an integer")
 
-    a = int(a)
-    b = int(b)
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
 
     return a + b
