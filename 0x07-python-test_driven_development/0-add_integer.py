eb9b957c5ceba6d18fcf09708a31c57b73aa4731
Leonard Nzekwe <nzekweleo@gmail.com>
2023-07-07 15:23:45 -0700

    Add Files, task-0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,12 +12,14 @@ def add_integer(a, b=98):
     """
     if (
         a is None or
+        not a or
         not isinstance(a, int) and
         not isinstance(a, float)
     ):
         raise TypeError("a must be an integer")
     if (
         b is None or
+        not b or
         not isinstance(b, int) and
         not isinstance(b, float)
     ):
