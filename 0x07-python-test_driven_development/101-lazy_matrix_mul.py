23a6910464be663243488a1f5ba11abd866943af
Leonard Nzekwe <nzekweleo@gmail.com>
2023-07-09 12:04:08 -0700

    Add Files, task-7

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -8,6 +8,7 @@ a function that multiplies 2 matrices by using the module NumPy
 
 import numpy as np
 
+
 def lazy_matrix_mul(m_a, m_b):
     """
     lazy_matrix_mul
@@ -16,10 +17,8 @@ def lazy_matrix_mul(m_a, m_b):
         """Convert the input matrices to NumPy arrays"""
         np_a = np.array(m_a)
         np_b = np.array(m_b)
-        
         """Perform matrix multiplication using np.matmul"""
         result = np.matmul(np_a, np_b)
-        
         return result
     except Exception as err:
         raise type(err)(str(err))
