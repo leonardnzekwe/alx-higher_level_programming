420d5339b6058a59041e19b7b148eb77ddb7846d
Leonard Nzekwe <nzekweleo@gmail.com>
2023-07-09 05:00:56 -0700

    Update Files, task-0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,20 +12,20 @@ def add_integer(a, b=98):
     """
     if (
         a is None or
-        not isinstance(a, int) and
-        not isinstance(a, float)
+        type(a) != int and
+        type(a) != float
     ):
         raise TypeError("a must be an integer")
     if (
         b is None or
-        not isinstance(b, int) and
-        not isinstance(b, float)
+        type(b) != int and
+        type(b) != float
     ):
         raise TypeError("b must be an integer")
 
-    if isinstance(a, float):
+    if type(a) == float:
         a = int(a)
-    if isinstance(b, float):
+    if type(b) == float:
         b = int(b)
 
     return a + b
---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+100-matrix_mul module
+That contains matrix_mul(m_a, m_b) function
+a function that multiplies 2 matrices
+"""
+
+
+def matrix_mul(m_a, m_b):
+    """
+    matrix_mul function
+    """
+    if (
+            m_a is None or
+            type(m_a) != list or
+
