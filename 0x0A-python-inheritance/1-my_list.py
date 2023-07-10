3880ee9a366447eb7d000b76dfa86259d37f1e80
Leonard Nzekwe <nzekweleo@gmail.com>
2023-07-10 06:04:47 -0700

    Add Files, task-1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+1-my_list Module
+contains print_sorted(self) function
+that prints the list, but sorted (ascending sort)
+"""
+
+
+class MyList(list):
+    """
+    MyList class
+    """
+    def print_sorted(self):
+        """
+        print_sorted function
+        """
+        new_list = sorted(self)
+        print(new_list)
