e1ff12030744ec070fab89378dbaf9488ed5b693
Leonard Nzekwe <nzekweleo@gmail.com>
2023-07-10 07:22:26 -0700

    Add Files, task-6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+6-base_geometry Module
+contains BaseGeometry class
+with its methods and attributes
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+    def area(self):
+        """
+        area function
+        """
+        raise Exception("area() is not implemented")
