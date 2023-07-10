ee9434dd74dcf903647d48904fb35ab220338e67
Leonard Nzekwe <nzekweleo@gmail.com>
2023-07-10 06:17:25 -0700

    Add Files, task-1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -10,9 +10,12 @@ class MyList(list):
     """
     MyList class
     """
-    def print_sorted(self):
-        """
-        print_sorted function
-        """
-        new_list = sorted(self)
-        print(new_list)
+    try:
+        def print_sorted(self):
+            """
+            print_sorted function
+            """
+            new_list = sorted(self)
+            print(new_list)
+    except Exception as err:
+        raise type(err)(str(err))
