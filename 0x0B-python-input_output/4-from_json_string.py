dfc1f9ddf230e1bdc8fd3da277d320ad41b46d12
Leonard Nzekwe <nzekweleo@gmail.com>
2023-07-11 07:26:43 -0700

    Add Files, task-4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+4-from_json_string function
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    from_json_string function
+    """
+    return json.loads(my_str)
