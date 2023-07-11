f2b70fa22ff186c0719dba99bf01f0f6fe761769
Leonard Nzekwe <nzekweleo@gmail.com>
2023-07-11 06:50:16 -0700

    Add Files, task-3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+3-to_json_string function
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    to_json_string function
+    """
+    return json.dumps(my_obj)
