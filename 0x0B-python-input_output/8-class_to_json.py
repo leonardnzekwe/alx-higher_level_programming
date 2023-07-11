26bd7d03337b69ff4cc440dd55fea5fa070c25ae
Leonard Nzekwe <nzekweleo@gmail.com>
2023-07-11 11:24:57 -0700

    Add Files, task-8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+8-class_to_json Module
+"""
+
+
+import json
+
+
+def class_to_json(obj):
+    """
+    class_to_json function
+    """
+    my_dict = {}
+    if hasattr(obj, "__dict__"):
+        for key, value in obj.__dict__.items():
+            my_dict.update({key: value})
+        return my_dict
