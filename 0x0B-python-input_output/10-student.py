8d4825b481ff1439f2cc4424fd567c77470361b7
Leonard Nzekwe <nzekweleo@gmail.com>
2023-07-11 12:03:46 -0700

    Add Files, task-9

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+9-student Module
+"""
+
+
+class Student:
+    """
+    student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        initialization method
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        to_json method
+        """
+        stu_dict = {}
+        for key, value in self.__dict__.items():
+            if attrs is not None:
+                for attr_key in attrs:
+                    if attr_key == key:
+                        stu_dict.update({attr_key: value})
+            else:
+                stu_dict.update({key: value})
+        return stu_dict
