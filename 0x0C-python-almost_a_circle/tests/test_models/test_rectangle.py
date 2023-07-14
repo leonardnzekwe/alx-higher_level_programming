9cb5ed5cb7e5ef156f6854d2f48e23c0b5ec823b
Leonard Nzekwe <nzekweleo@gmail.com>
2023-07-14 14:42:03 -0700

    Add Files, task-2

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -24,20 +24,39 @@ class TestRectangle(unittest.TestCase):
         test_rectangle_nb_objects method
         """
         r2 = Rectangle(2, 10)
-        self.assertEqual(r2.id, 3)
+        r3 = Rectangle(10, 2)
+        self.assertNotEqual(r2.id, r3.id)
 
     def test_rectangle_custom_id(self):
         """
         test_rectangle_custom_id method
         """
         custom_id = 12
-        r3 = Rectangle(10, 2, 0, 0, custom_id)
-        self.assertEqual(r3.id, 12)
+        r4 = Rectangle(10, 2, 0, 0, custom_id)
+        self.assertEqual(r4.id, 12)
 
     def test_rectangle_attr(self):
         """
         test_rectangle_attr method
         """
-        r4 = Rectangle(12, 23)
-        self.assertEqual(r4.width, 12)
-        self.assertEqual(r4.height, 23)
+        r5 = Rectangle(12, 23)
+        self.assertEqual(r5.width, 12)
+        self.assertEqual(r5.height, 23)
+
+    def test_rectangle_raises(self):
+        """
+        test_rectangle_raises method
+        """
+        with self.assertRaises(TypeError):
+            r6 = Rectangle(10, "2")
+
+        with self.assertRaises(ValueError):
+            r7 = Rectangle(10, 2)
+            r7.width = -10
+
+        with self.assertRaises(TypeError):
+            r8 = Rectangle(10, 2)
+            r8.x = {}
+
+        with self.assertRaises(ValueError):
+            r9 = Rectangle(10, 2, 3, -1)
