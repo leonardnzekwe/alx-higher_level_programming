f9b5838b74676b349054d37563aac04eb41971c2
Leonard Nzekwe <nzekweleo@gmail.com>
2023-07-14 15:29:17 -0700

    Add Files, task-3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -92,3 +92,9 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """
+        area method
+        """
+        return self.width * self.height
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -12,51 +12,71 @@ class TestRectangle(unittest.TestCase):
     """
     TestRectangle class
     """
+    @classmethod
+    def setUpClass(cls):
+        """
+        setUpClass method
+        """
+        cls.r1 = Rectangle(10, 2)
+        cls.r2 = Rectangle(2, 5)
+        cls.r3 = Rectangle(10, 2, 0, 0, 12)
+        cls.r4 = Rectangle(12, 23)
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        tearDownClass method
+        """
+        del cls.r1, cls.r2, cls.r3, cls.r4
+
+
     def test_rectangle_not_none(self):
         """
         test_rectangle_not_none method
         """
-        r1 = Rectangle(10, 2)
-        self.assertIsNotNone(r1.id)
+        self.assertIsNotNone(self.r1.id)
+        self.assertIsNotNone(self.r2.id)
 
     def test_rectangle_with_defaults(self):
         """
         test_rectangle_nb_objects method
         """
-        r2 = Rectangle(2, 10)
-        r3 = Rectangle(10, 2)
-        self.assertNotEqual(r2.id, r3.id)
+        self.assertNotEqual(self.r2.id, self.r3.id)
 
     def test_rectangle_custom_id(self):
         """
         test_rectangle_custom_id method
         """
-        custom_id = 12
-        r4 = Rectangle(10, 2, 0, 0, custom_id)
-        self.assertEqual(r4.id, 12)
+        self.assertEqual(self.r3.id, 12)
 
     def test_rectangle_attr(self):
         """
         test_rectangle_attr method
         """
-        r5 = Rectangle(12, 23)
-        self.assertEqual(r5.width, 12)
-        self.assertEqual(r5.height, 23)
+        self.assertEqual(self.r4.width, 12)
+        self.assertEqual(self.r4.height, 23)
 
     def test_rectangle_raises(self):
         """
         test_rectangle_raises method
         """
         with self.assertRaises(TypeError):
-            r6 = Rectangle(10, "2")
+            Rectangle(10, "2")
 
         with self.assertRaises(ValueError):
-            r7 = Rectangle(10, 2)
-            r7.width = -10
+            r = Rectangle(10, 2)
+            r.width = -10
 
         with self.assertRaises(TypeError):
-            r8 = Rectangle(10, 2)
-            r8.x = {}
+            r = Rectangle(10, 2)
+            r.x = {}
 
         with self.assertRaises(ValueError):
-            r9 = Rectangle(10, 2, 3, -1)
+            r = Rectangle(10, 2, 3, -1)
+
+    def test_rectangle_area(self):
+        """
+        test_rectangle_area method
+        """
+        self.assertEqual(self.r1.area(), 20)
+        self.assertEqual(self.r2.area(), 10)
