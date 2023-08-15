6fd065fc1149a754ad3d0f50d329da70fc2094ed
Leonard Nzekwe <nzekweleo@gmail.com>
2023-08-15 18:35:44 +0100

    Add Files, task-7

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- a script that inserts a new row in the table first_table (database hbtn_0c_0) in one's MySQL server
+-- Inserts a row with value into `first_table` table
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
