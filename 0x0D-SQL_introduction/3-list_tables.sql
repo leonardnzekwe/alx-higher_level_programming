592e9385179d37e380f2a574348ca4a143c37bd2
Leonard Nzekwe <nzekweleo@gmail.com>
2023-08-15 16:56:34 +0100

    Add Files, task-3

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,5 @@
+-- A script that lists all the tables of a database in one's MySQL server
+-- Query to list all the tables of the current database
+SELECT TABLE_NAME
+FROM information_schema.TABLES
+WHERE TABLE_SCHEMA = DATABASE();
