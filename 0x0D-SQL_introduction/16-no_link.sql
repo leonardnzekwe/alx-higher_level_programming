5294d21d52b0baee12ac02f1d1f63946ee3cfc3b
Leonard Nzekwe <nzekweleo@gmail.com>
2023-08-15 22:32:35 +0100

    Fix Statement Format

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,6 @@
 -- a script that lists all records of the table second_table of the database hbtn_0c_0 in one's MySQL server
 -- Retrieves rows with `name` value in the `second_table` table in the current database and sort them in descending order
-SELECT `score`, `name` FROM `second_table`
+SELECT `score`, `name`
+FROM `second_table`
 WHERE `name` != ""
 ORDER BY `score` DESC;
