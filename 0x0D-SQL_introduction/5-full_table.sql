25d204d198ca1ba9bdb3071be53770a4e4b0ef36
Leonard Nzekwe <nzekweleo@gmail.com>
2023-08-15 17:37:34 +0100

    Add Files, task-5

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- a script that prints the full description of the table first_table from the database hbtn_0c_0 in one's MySQL server
+-- Retrieve the create table SQL statement for first_table
+SHOW CREATE TABLE first_table;
