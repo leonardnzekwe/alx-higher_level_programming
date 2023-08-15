969503cc05991c10b3818b1259424ab396d7bd0d
Leonard Nzekwe <nzekweleo@gmail.com>
2023-08-15 18:30:18 +0100

    Add Files, task-6

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- a script that lists all rows of the table first_table from the database hbtn_0c_0 in one's MySQL server
+-- Retrieves all the rows in `first_table` table in the current database
+SELECT * FROM `first_table`;
