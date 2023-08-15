9da23b2a6826dad957f9f8165619dd8bf90144ca
Leonard Nzekwe <nzekweleo@gmail.com>
2023-08-15 19:03:09 +0100

    Add Files, task-10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- a script that lists all records of the table second_table of the database hbtn_0c_0 in one's MySQL server
+-- Retrieves (`score`, `name`) and sorts in descending order the rows in `second_table` table in the current database
+SELECT `score`, `name` FROM `second_table` ORDER BY `score` DESC;
