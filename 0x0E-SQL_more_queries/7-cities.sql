f4645f3c13f2b047b2cdb49985d73df279413b85
Leonard Nzekwe <nzekweleo@gmail.com>
2023-08-16 18:02:57 +0100

    Add Files, task-7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- a script that creates the database `hbtn_0d_usa` in one's MySQL server
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+
+-- and creates the table `cities` (in the database hbtn_0d_usa)
+USE `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `cities` (
+	`id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	`state_id` INT NOT NULL,
+	FOREIGN KEY (`state_id`) REFERENCES `states` (`id`),
+	`name` VARCHAR(256) NOT NULL
+);
