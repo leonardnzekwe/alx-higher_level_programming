808b344cc274c8d049e7e3415641d38b0c8a0287
Leonard Nzekwe <nzekweleo@gmail.com>
2023-08-16 16:56:47 +0100

    Add Files, task-3

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- a script that creates the table `force_name` on the current database in one's MySQL server
+CREATE TABLE IF NOT EXISTS `force_name` (
+	`id` INT,
+	`name` VARCHAR(256) NOT NULL
+);
