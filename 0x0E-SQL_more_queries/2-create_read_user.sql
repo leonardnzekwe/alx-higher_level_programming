386c1e960016e58b4f185a369a0e6b07c7d75c74
Leonard Nzekwe <nzekweleo@gmail.com>
2023-08-16 16:46:15 +0100

    Add Files, task-2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 
 -- create user 'user_0d_2'
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_PWD';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- and grants SELECT priviledge to 'user_0d_2'
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
