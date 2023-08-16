85f531818f8efb20ea5657804ba5dd0f00fbbaf4
Leonard Nzekwe <nzekweleo@gmail.com>
2023-08-16 16:30:33 +0100

    Add Files, task-1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,6 @@
 --  a script that creates the MySQL server user user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+
+-- and grants all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
