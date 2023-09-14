481bf13bc265696c7978a6053847e902075f9bc5
Leonard Nzekwe <nzekweleo@gmail.com>
2023-09-14 16:00:04 +0100

    Add Files, task-0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+0-select_states Module
+"""
+
+
+from sys import argv
+import MySQLdb
+
+
+def main():
+    """
+    main function
+    """
+    argc = len(argv) - 1
+    if (argc == 3):
+        mysql_user = argv[1]
+        mysql_pwd = argv[2]
+        db_name = argv[3]
+
+        conn = MySQLdb.connect(
+                host="localhost", port=3306, user=mysql_user,
+                passwd=mysql_pwd, db=db_name, charset="utf8"
+            )
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        conn.close()
+
+
+if __name__ == '__main__':
+    main()
